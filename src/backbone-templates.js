3b1039b29b9e32f59a377168be305bd9289ddb19
Yaroslav Ulanovych <y.ulanovych@thenewmotion.com>
2012-06-11 15:04:32 +0300

    no comments

--- src/backbone-templates.js
+++ src/backbone-templates.js
@@ -1,35 +1,56 @@
 Backbone.Templates = {
 	bind : function(template, data) {
-		if (template.length > 1) throw "single node required";
-		//change context
-		template.attr("data") && (data = data.get(template.attr("data")))
 	
-		if (template.children().length == 0) {
-			// substitute content
-			var text = template.text();
-			if (text[0] == "$") {
-				var variable = text.substring(1);
-				data.on("change:" + variable, _(function() {
-					template.text(data.get(variable));
-				}).tap(function(f){f.call()}));
-			}
-			// substitute attributes
-			_(template[0].attributes).each(function(attr) {
-				if (attr.value[0] == "$") {
-					var variable = attr.value.substring(1);
-					data.on("change:" + variable, _(function() {
-						template.attr(attr.name, data.get(variable));
-					}).tap(function(f)	{f.call()}));
-				}
-			});
-		} else {
-			
-			
-			// recursively bind all descendant nodes
-			template.children().each(function(index, node) {
-				Backbone.Templates.bind($(node), data);
+		// if the template consists of several nodes in a sequence,
+		// then bind each node separately to the same data
+		if (template.length > 1) {
+			template.each(function(index, item) {
+				Backbone.Templates.bind($(item), data);
 			});
 		}
+		
+		//change context
+		template.attr("data") && (data = data.get(template.attr("data")))
+		
+		if (data instanceof Backbone.Collection) {
+			data.bind("reset", function() {
+				
+			});
+			data.each(function(model) {
+				template.append(Backbone.Templates.bind(this.clone(), model));
+			}, template.children().detach());
+			
+		} else if (data instanceof Backbone.Model) {
+			if (template.children().length == 0) {
+				// substitute content
+				var text = template.text();
+				if (text[0] == "$") {
+					var variable = text.substring(1);
+					data.on("change:" + variable, _(function() {
+						template.text(data.get(variable));
+					}).tap(function(f){f.call()}));
+				}
+				// substitute attributes
+				_(template[0].attributes).each(function(attr) {
+					if (attr.value[0] == "$") {
+						var variable = attr.value.substring(1);
+						data.on("change:" + variable, _(function() {
+							template.attr(attr.name, data.get(variable));
+						}).tap(function(f)	{f.call()}));
+					}
+				});
+				return template;
+			} else {
+				// recursively bind all descendant nodes to the same data
+				template.children().each(function(index, node) {
+					Backbone.Templates.bind($(node), data);
+				});
+			}
+		} else {
+			throw "model/collection required"
+		}
+	
+		
 		
 	}
 }
--- test/backbone-templates-test.js
+++ test/backbone-templates-test.js
@@ -1,5 +1,22 @@
 describe("Bacbone.Templates", function() {
 
+	function backbonize(obj) {
+		var callee = arguments.callee;
+		if (_.isArray(obj)) {
+			return new Backbone.Collection(_(obj).map(function(item) {
+				return callee(item);
+			}));
+		} else if (_.isObject(obj)) {
+			var result = {};
+			_(obj).chain().keys().each(function(key) {
+				result[key] = callee(obj[key]);
+			});
+			return new Backbone.Model(result);
+		} else {
+			return obj;
+		}
+	}
+
 	describe("should substitute", function() {
 		
 		describe("content placeholders", function() {
@@ -19,7 +36,18 @@ describe("Bacbone.Templates", function() {
 				expect(template.children("div").text()).toBe("Jack");
 				model.set({name: "John"});
 				expect(template.children("div").text()).toBe("John");
-			});			
+			});
+			
+			it("in sibling nodes", function() {
+				var template = $("<span>$name</span><span>$name</span>");
+				var model = new Backbone.Model({name: "Jack"});
+				Backbone.Templates.bind(template, model);
+				expect($(template[0]).text()).toBe("Jack");
+				expect($(template[1]).text()).toBe("Jack");
+				model.set({name: "John"});
+				expect($(template[0]).text()).toBe("John");
+				expect($(template[1]).text()).toBe("John");
+			});
 
 		});
 		
@@ -41,25 +69,74 @@ describe("Bacbone.Templates", function() {
 				model.set({id: 2});
 				expect(template.children("span").attr("id")).toBe("2");
 			});
-		});
-		
-	
-	});
-	
-	describe("change context", function() {
-		it("", function() {
-			var template = $("<span data='car'>$color</span>");
-			Backbone.Templates.bind(template, new Backbone.Model({car: new Backbone.Model({color: "red"})}));
-			expect(template.text()).toBe("red");
-		});
-		
-	});
-	
-	describe("", function() {
-		it("", function() {
-			var template = $("<span></span>");
 			
+			it("in sibling nodes", function() {
+				var template = $("<span id='$id'></span><span id='$id'></span>");
+				var model = new Backbone.Model({id: "1"});
+				Backbone.Templates.bind(template, model);
+				expect($(template[0]).attr("id")).toBe("1");
+				expect($(template[1]).attr("id")).toBe("1");
+				model.set({id: 2});
+				expect($(template[0]).attr("id")).toBe("2");
+				expect($(template[1]).attr("id")).toBe("2");
+			});
 		});
+		
+	
+	});
+	
+	describe("should render nested", function() {
+		it("models", function() {
+			var template = $("<span data='car'>$color</span>");
+			var model = backbonize({car : {color : "red"}});
+			Backbone.Templates.bind(template, model);
+			expect(template.text()).toBe("red");
+			model.get("car").set("color", "red2");
+			expect(template.text()).toBe("red2");
+		});
+		
+		it("collections", function() {
+			var template = $("<div><ul data='cars'><li>$color</li></ul></div>");
+			var model = backbonize({cars : [{color: "red"}, {color: "green"}]});
+			Backbone.Templates.bind(template, model);
+			expect(template.find("ul > li:nth-child(1)").text()).toBe("red");
+			expect(template.find("ul > li:nth-child(2)").text()).toBe("green");
+			model.get("cars").at(0).set("color", "red2");
+			model.get("cars").at(1).set("color", "green2");
+			expect(template.find("ul > li:nth-child(1)").text()).toBe("red2");
+			expect(template.find("ul > li:nth-child(2)").text()).toBe("green2");
+		});
+	});
+
+		
+	describe("while rendering collections", function() {
+		it("should bind templates to models", function() {
+			var template = $("<div><span>$name</span></div>");
+			var collection = new Backbone.Collection([{name: 1}, {name: 2}, {name: 3}]);
+			Backbone.Templates.bind(template, collection);
+			expect(template.children("span:nth-child(1)").text()).toBe("1");
+			expect(template.children("span:nth-child(2)").text()).toBe("2");
+			expect(template.children("span:nth-child(3)").text()).toBe("3");
+			collection.at(0).set({name: 4});
+			collection.at(1).set({name: 5});
+			collection.at(2).set({name: 6});
+			expect(template.children("span:nth-child(1)").text()).toBe("4");
+			expect(template.children("span:nth-child(2)").text()).toBe("5");
+			expect(template.children("span:nth-child(3)").text()).toBe("6");
+		});
+		
+		describe("should react on add event", function() {
+			it("when adding to the end", function() {
+				var template = $("<div><span>$name</span></div>");
+				var collection = backbonize([{name: 1}, {name: 2}]);
+				Backbone.Templates.bind(template, collection);
+				collection.add({name: 3});
+				expect(template.children("span:nth-child(3)").text()).toBe("3");
+			});
+		});
+	});
+	
+	xit("should render complex templates with nested models and collections", function() {
 	});
 	
 });
